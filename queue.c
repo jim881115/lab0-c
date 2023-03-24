1874ae4f0faffef1b6266b88e977fbb0515ea58c
jim881115 <jimleejongsuk@gmail.com>
2023-03-24 21:50:27 +0800

    Add q_delete_dup function

--- queue.c
+++ queue.c
@@ -111,7 +111,19 @@ bool q_delete_mid(struct list_head *head)
 /* Delete all nodes that have duplicate string */
 bool q_delete_dup(struct list_head *head)
 {
-    // https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
+    if (!head)
+        return false;
+    element_t *entry, *safe;
+    char *tmp = malloc(sizeof(char));
+    list_for_each_entry_safe (entry, safe, head, list) {
+        if (&safe->list != head && !strcmp(entry->value, safe->value))
+            tmp = strdup(entry->value);
+        if (!strcmp(tmp, entry->value)) {
+            list_del(&entry->list);
+            q_release_element(entry);
+        }
+    }
+    free(tmp);
     return true;
 }
 
